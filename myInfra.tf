c668d145b6745ffb0a2fd4a3f637df1889186207
Ameen Naseef PT <141341385+ameenptmpd@users.noreply.github.com>
2023-11-27 19:55:04 +0530

    Update myInfra.tf

--- myInfra.tf
+++ myInfra.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "docker_instance" {
+resource "aws_instance" "my_instance" {
   count         = 1       #10 instances at a time
   ami           = "ami-0430580de6244e02e"
   instance_type = "t2.micro"
