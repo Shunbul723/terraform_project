0b85ae4842f84fe23b883395a3fb2da54e2d959e
Ameen Naseef PT <141341385+ameenptmpd@users.noreply.github.com>
2023-11-27 10:40:28 +0530

    Create myInfra.tf

---
+++ myInfra.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "docker_instance" {
+  count         = 1       #10 instances at a time
+  ami           = "ami-0430580de6244e02e"
+  instance_type = "t2.micro"
+  key_name      = "my_key"
+
+security_groups=["launch-wizard"]
+  tags = {
+    Name = "DockerInstance"
+  }
+}
