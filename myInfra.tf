267636b8f48bc1fcb20f78d684a04d7858af398c
Shunbul723 <143285228+Shunbul723@users.noreply.github.com>
2023-12-02 00:32:19 +0530

    Update myInfra.tf

--- myInfra.tf
+++ myInfra.tf
@@ -6,9 +6,9 @@ resource "aws_instance" "my_instance" {
   count         = 1       #10 instances at a time
   ami           = "ami-0430580de6244e02e"
   instance_type = "t2.micro"
-  key_name      = "my_key"
+  key_name      = "Faizan Keypair"
 
-security_groups=["launch-wizard"]
+security_groups=["launch-wizard-1"]
   tags = {
     Name = "DockerInstance"
   }
